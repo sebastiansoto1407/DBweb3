69c8c587da24d948a7de5dd19ec660c68c580f27
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-08-26 21:02:05 -0400

    faltamigrar

---
+++ crudsweb3/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace crudsweb3.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string nombreTarea { get; set; }
+        public DateTime fechaVencimiento { get; set; }
+        public String estado { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ crudsweb3/data/TareaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crudsweb3.Models;
+
+namespace crudsweb3.data
+{
+    public class TareaDbContext :  DbContext
+    {
+        public TareaDbContext(DbContextOptions <TareaDbContext> options) : base(options) {
+            
+        }
+        public DbSet<Tarea> Tareas {  get; set; }
+
+        protected TareaDbContext()
+        {
+
+        }
+    }
+}
